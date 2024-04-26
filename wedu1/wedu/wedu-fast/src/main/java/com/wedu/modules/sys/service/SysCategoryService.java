ebde240158372b646cfe40bbdf891a006fc95134
hdqwer <101729423+hdqwer@users.noreply.github.com>
2024-04-26 15:23:52 +0800

    Delete wedu1/wedu directory

--- wedu1/wedu/wedu-fast/src/main/java/com/wedu/modules/sys/service/SysCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.wedu.modules.sys.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wedu.modules.sys.entity.CategoryEntity;
-import com.wedu.modules.sys.entity.GoodsEntity;
-
-public interface SysCategoryService extends IService<CategoryEntity> {
-}
